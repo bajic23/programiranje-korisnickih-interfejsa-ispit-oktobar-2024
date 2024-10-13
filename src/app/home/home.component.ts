11d919598f94c6b443c7f9920adb4559b0e117d8
bajic23 <pbajic03@gmail.com>
2024-10-13 18:06:32 +0200

    Update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,15 @@ import { Component } from '@angular/core';
 import {ChangeDetectionStrategy} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, HttpClientModule],
+  imports: [MatCardModule, MatButtonModule,MatListModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-public recommended: any[] | undefined = undefined
-
-  constructor(client: HttpClient){}
 }
