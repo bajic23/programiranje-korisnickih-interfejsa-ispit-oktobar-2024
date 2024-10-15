731aee5ff9e2fd39131ebfc3ab69c400244694d1
bajic23 <pbajic03@gmail.com>
2024-10-15 12:09:39 +0200

    Added models

---
+++ src/models/movie.models.ts
@@ -0,0 +1,11 @@
+export interface MovieModel{
+    id: number
+    director: string
+    actors: string
+    genre: string
+    duration: string
+    year_of_making: number
+    time_date: string
+    price: string
+    rateing: string
+}
