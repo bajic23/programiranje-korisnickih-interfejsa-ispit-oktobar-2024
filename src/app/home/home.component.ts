2086440951a2f598c6ab27ecf5c94b6f0129222e
bajic23 <pbajic03@gmail.com>
2024-10-15 14:07:12 +0200

    Updated movies catalog

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+//import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {ChangeDetectionStrategy} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,MatListModule],
+  imports: [MatCardModule, MatButtonModule,MatListModule,MatFormFieldModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
