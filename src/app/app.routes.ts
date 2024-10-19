705aeb802b50cffbdbef672a60c7c467417c120a
bajic23 <pbajic03@gmail.com>
2024-10-19 15:48:22 +0200

    Added login and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
 import { MovieComponent } from './movie/movie.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    {path: 'search', component: SearchComponent},
-    {path: 'movie', component: MovieComponent},
+    { path: 'search', component: SearchComponent },
+    { path: 'movie', component: MovieComponent },
+    { path: 'signup', component: SignupComponent },
+    {path: 'login', component:LoginComponent},
     { path: '**', redirectTo: '' }
 ];
